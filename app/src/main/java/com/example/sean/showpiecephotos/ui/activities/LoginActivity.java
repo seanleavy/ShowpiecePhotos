775858500c641c399c11fb618fe0d5ac0e796f60
sean <seanleavy@gmail.com>
2017-06-09 17:29:54 +0100

    Hello Wicklow

---
+++ app/src/main/java/com/example/sean/showpiecephotos/ui/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.sean.showpiecephotos.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sean.showpiecephotos.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
