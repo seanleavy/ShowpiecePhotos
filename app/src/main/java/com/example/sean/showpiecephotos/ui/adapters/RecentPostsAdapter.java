c722bc1c04427d6a30089ae5f3efae7af5c1a4f1
sean <seanleavy@gmail.com>
2017-06-11 22:42:02 +0100

    Added rudimentary custom RecyclerView Adapter

--- app/src/main/java/com/example/sean/showpiecephotos/ui/adapters/RecentPostsAdapter.java
+++ app/src/main/java/com/example/sean/showpiecephotos/ui/adapters/RecentPostsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.sean.showpiecephotos.ui.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sean.showpiecephotos.model.Pojo.recentPosts.InstagramPost;
@@ -9,22 +10,44 @@ import com.example.sean.showpiecephotos.model.Pojo.recentPosts.nested.Datum;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by sean on 11/06/2017.
- */
+import butterknife.ButterKnife;
 
-//public class RecentPostsAdapter extends RecyclerView.Adapter<RecentPostsAdapter.ViewHolder> {
-//
-//    // Used Datum part of model and split the InstagramPost response
-//    private List<Datum> recentPosts;
-//
-//    public RecentPostsAdapter() {
-//        this.recentPosts = new ArrayList<>();
-//    }
-//
-//    public void addPost(InstagramPost instagramPost) {
-//        recentPosts.addAll(instagramPost.getData());
-//        notifyDataSetChanged();
-//    }
+
+public class RecentPostsAdapter extends RecyclerView.Adapter<RecentPostsAdapter.ViewHolder> {
+
+    // Used Datum part of model and split the InstagramPost response
+    private List<Datum> recentPosts;
+
+    public RecentPostsAdapter() {
+        this.recentPosts = new ArrayList<>();
+    }
+
+    public void addPost(InstagramPost instagramPost) {
+        recentPosts.addAll(instagramPost.getData());
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return recentPosts.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
 
 }
