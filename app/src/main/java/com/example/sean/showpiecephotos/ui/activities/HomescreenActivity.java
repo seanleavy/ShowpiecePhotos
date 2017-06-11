bad3745762aff0ea20fee76c243e67311b53152e
sean <seanleavy@gmail.com>
2017-06-12 00:54:07 +0100

    added layout for list items.  more additions to recyclerview viewholder. fleshed out layout for user self on homescreen

--- app/src/main/java/com/example/sean/showpiecephotos/ui/activities/HomescreenActivity.java
+++ app/src/main/java/com/example/sean/showpiecephotos/ui/activities/HomescreenActivity.java
@@ -2,12 +2,13 @@ package com.example.sean.showpiecephotos.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
-import com.example.sean.showpiecephotos.constants.Constants;
 import com.example.sean.showpiecephotos.R;
 import com.example.sean.showpiecephotos.events.RecentPostsEvent;
+import com.example.sean.showpiecephotos.ui.adapters.RecentPostsAdapter;
 import com.example.sean.showpiecephotos.ui.presenters.HomescreenPresenter;
 import com.example.sean.showpiecephotos.ui.presenters.HomescreenPresenterImpl;
 import com.example.sean.showpiecephotos.ui.views.HomescreenView;
@@ -21,21 +22,28 @@ import butterknife.ButterKnife;
 
 public class HomescreenActivity extends AppCompatActivity implements HomescreenView {
 
-    private HomescreenPresenter homescreenPresenter;
+    @BindView(R.id.recent_posts_recyclerview)
+    RecyclerView recentPostsRecyclerView;
 
-    @BindView(R.id.access_token)
-    TextView mAccessToken;
+    private HomescreenPresenter homescreenPresenter;
+    private RecentPostsAdapter recentPostsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
         ButterKnife.bind(this);
-        mAccessToken.setText(getIntent().getExtras().getString(Constants.SHARED_PREFERENCE_TOKEN));
+//        mAccessToken.setText(getIntent().getExtras().getString(Constants.SHARED_PREFERENCE_TOKEN));
+        setUpRecyclerView();
         homescreenPresenter = new HomescreenPresenterImpl(this);
         homescreenPresenter.populateUser();
         homescreenPresenter.getRecentPosts();
+    }
 
+    private void setUpRecyclerView() {
+        recentPostsRecyclerView.setLayoutManager(new LinearLayoutManager(recentPostsRecyclerView.getContext()));
+        recentPostsAdapter = new RecentPostsAdapter();
+        recentPostsRecyclerView.setAdapter(recentPostsAdapter);
     }
 
     @Override
@@ -52,7 +60,7 @@ public class HomescreenActivity extends AppCompatActivity implements HomescreenV
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onRecentPostsSuccessEvent(RecentPostsEvent recentPostsEvent) {
-        //adadpter here
+        recentPostsAdapter.addPost(recentPostsEvent.getRecentPosts());
         Toast.makeText(this, "event caught", Toast.LENGTH_SHORT).show();
     }
 
--- app/src/main/java/com/example/sean/showpiecephotos/ui/adapters/RecentPostsAdapter.java
+++ app/src/main/java/com/example/sean/showpiecephotos/ui/adapters/RecentPostsAdapter.java
@@ -1,15 +1,21 @@
 package com.example.sean.showpiecephotos.ui.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.sean.showpiecephotos.R;
+import com.example.sean.showpiecephotos.R2;
 import com.example.sean.showpiecephotos.model.Pojo.recentPosts.InstagramPost;
 import com.example.sean.showpiecephotos.model.Pojo.recentPosts.nested.Datum;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
@@ -29,12 +35,17 @@ public class RecentPostsAdapter extends RecyclerView.Adapter<RecentPostsAdapter.
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recent_posts_row_item, parent, false);
+        ViewHolder recentPostsViewholder = new ViewHolder(v);
+        return recentPostsViewholder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.caption.setText(recentPosts.get(position).getCaption().getText());
+        holder.fullName.setText(recentPosts.get(position).getUser().getFullName());
+        holder.likes.setText(recentPosts.get(position).getLikes().getCount().toString());
+        //populate widget values here with datum.get
     }
 
     @Override
@@ -43,6 +54,18 @@ public class RecentPostsAdapter extends RecyclerView.Adapter<RecentPostsAdapter.
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.post_image)
+        ImageView postImage;
+
+        @BindView(R.id.post_fullname)
+        TextView fullName;
+
+        @BindView(R.id.post_likes)
+        TextView likes;
+
+        @BindView(R.id.post_caption_text)
+        TextView caption;
 
         public ViewHolder(View itemView) {
             super(itemView);
